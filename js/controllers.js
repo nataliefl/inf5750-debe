c8c1157885daff87295ad8b58d0b7bf1636ee1c9
Shayan Yazdanmehr <shayany@ifi.uio.no>
2014-12-08 05:56:49 +0100

    Fix errors which Thrown by delete and save functions

--- js/controllers.js
+++ js/controllers.js
@@ -36,50 +36,53 @@ controller('ListController', ['$scope','DataElements', '$location' , '$routePara
   $scope.delete=function(id)
   {
     DataElements.delete({'id' : id});
+      getPage($scope.currentPage);
+      location.reload();
   };
   $scope.save=function(name,shorName,code,description,domainType,valueType,numberType,aggregationOperator,
         storeZeroDataValues,url,categoryCombination,optionSetForDataValues,optionSetForComments,legendSets,rationale,unitMeasure)
   {
-      $scope.newData.name=name;
-      $scope.newData.shortName=shorName;
-      $scope.newData.code=code;
-      $scope.newData.description=description;
-      $scope.newData.domainType=domainType;
-      $scope.newData.numberType=numberType;
-      $scope.newData.aggregationOperator=aggregationOperator;
-      $scope.newData.zeroIsSignificant=storeZeroDataValues;
-      $scope.newData.url=url;
-      $scope.newData.categoryCombo.name=categoryCombination;
-      $scope.newData.optionSet.name=optionSetForDataValues
-      $scope.newData.commentOptionSet.name=optionSetForComments;
+      $scope.newData.name=(name)?name:"";
+      $scope.newData.shortName=(shorName)?shorName:"";
+      $scope.newData.code=(code)?code:"";
+      $scope.newData.description=(description)?description:"";
+      $scope.newData.domainType=(domainType)?domainType:"";
+      $scope.newData.numberType=(numberType)?numberType:"";
+      $scope.newData.aggregationOperator=(aggregationOperator)?aggregationOperator:"";
+      $scope.newData.zeroIsSignificant=(storeZeroDataValues)?storeZeroDataValues:"";
+      $scope.newData.url=(url)?url:"";
+      $scope.newData.categoryCombo=(categoryCombination)?{"name":categoryCombination}:{"name":""};
+      $scope.newData.optionSet=(optionSetForDataValues)?{"name":optionSetForDataValues}:{"name":""};
+      $scope.newData.commentOptionSet=(optionSetForComments)?{"name":optionSetForComments}:{"name":""};
       //$scope.newData.legendSet=legendSets;
-      $scope.newData.attributeValues[0].value=rationale;
-      $scope.newData.attributeValues[1].value=unitMeasure;
+      $scope.newData.attributeValues[0]=(rationale)?{"value":rationale}:{"value":""};
+      $scope.newData.attributeValues[1]=(unitMeasure)?{"value":unitMeasure}:{"value":""};
       DataElements.save($scope.newData);
       getPage($scope.currentPage);
+      location.reload();
   };
 
   $scope.getDetails=function(id){
       DataElements.retrieveDetails({'id' : id}).$promise.then(function(data){
           $scope.openedItem = data;
-          $scope.nameInput = $scope.openedItem.name;
-          $scope.shortNameInput=$scope.openedItem.shortName;
-          $scope.codeInput=$scope.openedItem.code;
-          $scope.descriptionTextArea=$scope.openedItem.description;
-          $scope.domainTypeSelector=$scope.openedItem.domainType;
-          $scope.valueTypeList=$scope.openedItem.type;
+          $scope.nameInput = ($scope.openedItem.name)?$scope.openedItem.name:"";
+          $scope.shortNameInput=($scope.openedItem.shortName)?$scope.openedItem.shortName:"";
+          $scope.codeInput=($scope.openedItem.code)?$scope.openedItem.code:"";
+          $scope.descriptionTextArea=($scope.openedItem.description)?$scope.openedItem.description:"";
+          $scope.domainTypeSelector=($scope.openedItem.domainType)?$scope.openedItem.domainType:"";
+          $scope.valueTypeList=($scope.openedItem.type)?$scope.openedItem.type:"";
           var textOrNumber;
           if($scope.openedItem.numberType!=null)
           {
-              $scope.numberType=$scope.openedItem.numberType;
+              $scope.numberType=($scope.openedItem.numberType)?$scope.openedItem.numberType:"";
               textOrNumber="numberType";
           }
           else
           {
-              $scope.numberType=$scope.openedItem.textType;
+              $scope.numberType=($scope.openedItem.textType)?$scope.openedItem.textType:"";
               textOrNumber="textType";
           }
-          $scope.aggregationOperatorList=$scope.openedItem.aggregationOperator;
+          $scope.aggregationOperatorList=($scope.openedItem.aggregationOperator)?$scope.openedItem.aggregationOperator:"";
           //$scope.storeZeroDataValuesList=$scope.openedItem.zeroIsSignificant;
           if($scope.openedItem.zeroIsSignificant==true)
           {
@@ -90,17 +93,17 @@ controller('ListController', ['$scope','DataElements', '$location' , '$routePara
               $scope.storeZeroDataValuesList="false";
           }
           $scope.urlInput=$scope.openedItem.url;
-          $scope.categoryCombinationList=$scope.openedItem.categoryCombo.name;
-          $scope.optionSetForDataValuesList=$scope.openedItem.optionSet.name;
-          $scope.optionSetForCommentsList=$scope.openedItem.commentOptionSet.name;
-          $scope.legendSetsList=$scope.openedItem.legendSet.name;
+          $scope.categoryCombinationList=($scope.openedItem.categoryCombo/*.name*/)?$scope.openedItem.categoryCombo.name:"";
+          $scope.optionSetForDataValuesList=($scope.openedItem.optionSet/*.name*/)?$scope.openedItem.optionSet.name:"";
+          $scope.optionSetForCommentsList=($scope.openedItem.commentOptionSet/*.name*/)?$scope.openedItem.commentOptionSet.name:"";
+          $scope.legendSetsList=($scope.openedItem.legendSet/*.name*/)?$scope.openedItem.legendSet.name:"";
           /*if($scope.openedItem.legendSet.name indexOf)*/
           $scope.nationalCheckbox=($scope.openedItem.aggregationLevels.indexOf(1)>=0)?true:false;
           $scope.districtCheckbox=($scope.openedItem.aggregationLevels.indexOf(2)>=0)?true:false;
           $scope.chiefdomCheckbox=($scope.openedItem.aggregationLevels.indexOf(3)>=0)?true:false;
           $scope.facilityCheckbox=($scope.openedItem.aggregationLevels.indexOf(4)>=0)?true:false;
-          $scope.rationaleInput=$scope.openedItem.attributeValues[0].value;
-          $scope.unitMeasureInput=$scope.openedItem.attributeValues[1].value;
+          $scope.rationaleInput=($scope.openedItem.attributeValues[0]/*.value*/)?$scope.openedItem.attributeValues[0].value:"";
+          $scope.unitMeasureInput=($scope.openedItem.attributeValues[1]/*.value*/)?$scope.openedItem.attributeValues[1].value:"";
           $scope.newData=data;
           /*var jsonObject={"id":id,
               "name":$scope.nameInput,
